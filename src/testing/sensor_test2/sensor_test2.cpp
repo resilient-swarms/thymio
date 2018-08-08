a6c72d2e85ad52d1f8566d21337326a5ba3315e7
SinaSarparast <ss4g17@soton.ac.uk>
2018-08-08 21:29:01 +0100

    added new experiment which tests sensors

---
+++ src/testing/sensor_test2/sensor_test2.cpp
@@ -0,0 +1,83 @@
+/* Include the controller definition */
+#include "sensor_test2.h"
+/* Function definitions for XML parsing */
+#include <argos3/core/utility/configuration/argos_configuration.h>
+/* 2D vector definition */
+#include <argos3/core/utility/math/vector2.h>
+
+#include <unistd.h>
+
+/****************************************/
+/****************************************/
+
+CSensorTest::CSensorTest() :
+   m_pcWheels(NULL),
+   m_pcProximity(NULL),
+   m_pcGround(NULL),
+   m_pcLeds(NULL),
+   m_cAlpha(10.0f),
+   m_fDelta(0.5f),
+   m_fWheelVelocity(2.5f),
+   m_cGoStraightAngleRange(-ToRadians(m_cAlpha),
+                           ToRadians(m_cAlpha)) {}
+
+/****************************************/
+/****************************************/
+
+void CSensorTest::Init(TConfigurationNode& t_node) {
+
+   m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+   m_pcLeds      = GetActuator<CCI_ThymioLedsActuator          >("thymio_led");
+   m_pcProximity = GetSensor  <CCI_ThymioProximitySensor       >("Thymio_proximity"     );
+   m_pcGround    = GetSensor  <CCI_ThymioGroundSensor          >("Thymio_ground");
+   m_tick        = 0;
+
+   try {
+       sensor_readings.open("sensor_readings.csv",std::ios_base::app);
+       sensor_readings<<",0cm";
+   } catch (std::exception e) {
+       std::cout << e.what();
+   }
+
+}
+
+/****************************************/
+/****************************************/
+
+void CSensorTest::ControlStep() {
+   /*Increase tick*/
+    m_tick++;
+
+   /* Get readings from proximity sensor */
+   const CCI_ThymioProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
+   /* Get readings from ground sensor */
+   const CCI_ThymioGroundSensor::TReadings& tGroundReads = m_pcGround->GetReadings();
+
+   sensor_readings <<","<<tProxReads[2].Value; // tick / 10 ticks per second * 1cm/s = distance
+
+   m_pcLeds->SetProxHIntensity(tProxReads);
+
+//   LOG << tGroundReads;
+   std::cout<< "Ground 1 "<< tGroundReads[0]<<"\n";
+   std::cout<< "Ground 2 "<< tGroundReads[1]<<"\n";
+   std::cout<< "proximity :"<< tProxReads[2].Value <<"\n";
+
+
+
+   m_pcWheels->SetLinearVelocity(0,0);
+}
+
+CSensorTest::~CSensorTest(){
+    try {
+        sensor_readings<<"\n";
+        sensor_readings.close();
+    } catch (std::exception e) {
+        std::cout << e.what();
+    }
+    m_pcWheels->SetLinearVelocity(0.0f, 0.0f);
+}
+
+/****************************************/
+/****************************************/
+
+REGISTER_CONTROLLER(CSensorTest, "sensor_test2_controller")
---
+++ src/testing/sensor_test2/sensor_test2.h
@@ -0,0 +1,62 @@
+#ifndef Thymio_DIFFUSION_H
+#define Thymio_DIFFUSION_H
+
+#include <argos3/core/utility/math/angles.h>
+#include <argos3/core/control_interface/ci_controller.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
+#include <argos3/plugins/robots/Thymio/control_interface/ci_Thymio_leds_actuator.h>
+#include <argos3/plugins/robots/Thymio/control_interface/ci_Thymio_proximity_sensor.h>
+#include <argos3/plugins/robots/Thymio/control_interface/ci_Thymio_ground_sensor.h>
+#include <argos3/core/utility/logging/argos_log.h>
+
+#include <iostream>
+#include <fstream>
+
+using namespace argos;
+
+class CSensorTest : public CCI_Controller {
+
+public:
+
+   /* Class constructor. */
+   CSensorTest();
+
+   /* Class destructor. */
+   virtual ~CSensorTest();
+
+   virtual void Init(TConfigurationNode& t_node);
+
+   virtual void ControlStep();
+
+   virtual void Reset() {}
+
+   virtual void Destroy(){}
+
+private:
+   /* Pointer to the LEDs */
+    CCI_ThymioLedsActuator*   m_pcLeds;
+   /* Pointer to the differential steering actuator */
+   CCI_DifferentialSteeringActuator* m_pcWheels;
+   /* Pointer to the Thymio proximity sensor */
+   CCI_ThymioProximitySensor* m_pcProximity;
+   /* Pointer to the Thymio ground sensors */
+   CCI_ThymioGroundSensor* m_pcGround;
+
+   CDegrees m_cAlpha;
+
+   Real m_fDelta;
+   /* Wheel speed. */
+   Real m_fWheelVelocity;
+
+   /*Simulator tick*/
+   Real m_tick;
+
+   /* Angle tolerance range to go straight.
+    * It is set to [-alpha,alpha]. */
+   CRange<CRadians> m_cGoStraightAngleRange;
+
+   std::ofstream sensor_readings;
+
+};
+
+#endif
